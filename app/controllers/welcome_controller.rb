b4e5f208fd9fd8b20fe35938334f768939ea9455
carolina <carolina.santiago.t@hotmail.com>
2020-12-16 15:04:59 +0000

    back to befor no tips

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,11 +4,7 @@ class WelcomeController < ApplicationController
 
   helper_method :random_tip
   def random_tip
-    if Tip.all.nil?
-      "No tips today"
-    else
-      @piqued_tip = Tip.all.sample.tip_description
-    end
+    @piqued_tip = Tip.all.sample.tip_description
   end 
 
 end
